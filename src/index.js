9c63ebacfec5b4b5dd2d189416cfc5a9c7d25612
jmilll <jwmiller.design@gmail.com>
2021-02-09 14:42:45 -0500

    tidy up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './normalize.css';
 import './style.css';
 import { getWeather } from './module/logic-functions';
 import { addEventListeners } from './module/event-listeners';
 
-
-console.log('weather-app');
-
-
-
 addEventListeners();
 
 getWeather('detroit', 'imperial');
-
-console.log('finish load');
-
-
-//export {allProjects, setData,};
